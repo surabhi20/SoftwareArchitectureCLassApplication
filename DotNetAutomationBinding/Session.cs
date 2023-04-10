70063e5878f2f2765c2769fe7bac20f6b3d931f4
surabhi20 <surabhidhariwal2010@gmail.com>
2023-04-10 19:27:59 -0400

    Update Session.cs
    Session.cs updated

--- DotNetAutomationBinding/Session.cs
+++ DotNetAutomationBinding/Session.cs
@@ -1,42 +1,57 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
+
+ 
 
 namespace DotNetAutomationBinding
 {
-    public class Session
+    public class Session
+    {
 
-    {
+        [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        static extern void DotNet_automationapi_Session_InitializeSession();
 
-        [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-        static extern void DotNet_automationapi_Session_InitializeSession();
+ 
 
-        [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-        static extern int DotNet_automationapi_Session_MakePart(string partPath);
+        [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        static extern int DotNet_automationapi_Session_MakePart(string partPath);
 
-
-        private Session()
-        {
-            DotNet_automationapi_Session_InitializeSession();
-        }
+ 
 
 
-        private static readonly Lazy<Session> lazy = new Lazy<Session>(() => new Session());
-        public static Session GetSession
-        {
-            get
-            {
-                return lazy.Value;
-            }
-        }
+        private Session()
+        {
+            DotNet_automationapi_Session_InitializeSession();
+        }
+
+ 
 
 
-        public Part MakePart(string pathName)
-        {
-            Part partpart = null;
+        private static readonly Lazy<Session> lazy = new Lazy<Session>(() => new Session());
+        public static Session GetSession
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
 
 
-            return partpart;
-        }
+        public Part MakePart(string pathName)
+        {
+            int guid = DotNet_automationapi_Session_MakePart(pathName);
+            if (guid == 0)
+            {
+                throw new Exception("Failed to create part.");
+            }
 
-    }
+ 
+
+            Part part = new Part(guid);
+            return part;
+        }
+
+ 
+
+    }
 }
