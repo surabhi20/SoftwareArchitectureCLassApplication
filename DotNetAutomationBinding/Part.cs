40f9b49429ebc3785713aaf8067b2b6317b9d50c
surabhi20 <surabhidhariwal2010@gmail.com>
2023-04-10 19:26:09 -0400

    Update Part.cs
    Part.cs updated

--- DotNetAutomationBinding/Part.cs
+++ DotNetAutomationBinding/Part.cs
@@ -1,30 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+ 
+
 namespace DotNetAutomationBinding
 {
-    public class Part
-    {
-        //TODO to setup the PINVOKE for save
+    public class Part
+    {
+        // TODO
+        [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl)]
+        static extern void DotNet_automationapi_Part_Save(int guid);
 
+        public void Save()
+        {
+            DotNet_automationapi_Part_Save(m_guid);
+        }
 
+ 
 
-        public void Save()
-        {
-            //TODO
+        public Part(int guid)
+        {
+            m_guid = guid;
+        }
 
-        }
+        private int m_guid;
 
-        public Part(int guid)  
-        {
-            m_guid = guid;
-        }
-
-        private int m_guid;
-
-    }
+    }
 }
